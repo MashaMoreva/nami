99d74b8872d976693ac5d1e8a436b215d25c1fc3
Masha Moreva <mashamiskova@yandex.ru>
2024-01-15 19:53:40 +0300

    create-default-layout

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <main>
+      <Nuxt />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main {
+  flex: 1;
+  overflow-y: auto;
+  // padding-top:;
+  // padding-bottom:;
+}
+</style>
+
+<script>
+import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue'
+
+export default {
+  components: { AppHeader, AppFooter }
+}
+</script>
