28c788bcd5e709f4901bb27f9badbbc0a84b4fcf
Masha Moreva <mashamiskova@yandex.ru>
2024-01-15 19:53:01 +0300

    create-app-footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+@import "~/assets/styles/_variables.scss";
+
+
+footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  min-height: 39px;
+  background-color: $bg-footer;
+}
+</style>
+
+
+<script>
+
+export default {
+}
+</script>
