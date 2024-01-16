268266d7792faf05b5b38f33037dcbfafb54e421
Masha Moreva <mashamiskova@yandex.ru>
2024-01-16 14:20:21 +0300

    create-products-list-component

---
+++ components/ProductsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="grid">
+    <ProductCard v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue';
+
+export default {
+  components: {
+    ProductCard,
+  },
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  grid-gap: 30px;
+  margin: 60px 0 100px;
+}
+</style>
