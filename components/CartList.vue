fb3b3f3da650e873fa15e71ac6f89fed5b6918e8
Masha Moreva <mashamiskova@yandex.ru>
2024-01-18 12:39:19 +0300

    create-cart-list-component

---
+++ components/CartList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex">
+    <CartItem v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<script>
+import CartItem from '@/components/CartItem.vue';
+
+export default {
+  components: {
+    CartItem,
+  },
+  computed: {
+    products() {
+      return this.$store.state.cartProducts;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  width: 100%;
+}
+</style>
