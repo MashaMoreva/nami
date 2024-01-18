fb6c948355878118cd42cb75835bad0552075fd0
Masha Moreva <mashamiskova@yandex.ru>
2024-01-18 12:39:31 +0300

    create-cart-component

---
+++ components/Cart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="cart-container">
+    <p class="title">Корзина</p>
+    <CartList />
+  </div>
+</template>
+
+<script>
+import CartList from '@/components/CartList.vue';
+
+export default {
+  components: {
+    CartList,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.cart-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 40px 40px 45px 45px;
+
+  .title {
+    margin-top: 0;
+    font-size: 48px;
+    margin-bottom: 30px;
+
+    &::before {
+      right: 206px;
+    }
+
+  }
+}
+</style>
